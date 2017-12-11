427ccde4180b07dba988e455309074acb7e09bdf
Justin Shrake <jshrake@oblong.com>
2017-12-11 09:20:42 -0800

    add setup script

---
+++ setup.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+
+ln -fs $(pwd)/.zshrc ${HOME}/.
+ln -fs $(pwd)/.vimrc ${HOME}/.
+ln -fs $(pwd)/.tmux.conf ${HOME}/.
+
+# install the pure prompt
+npm install --global pure-prompt
+
+# install zsh nice to haves
+brew install zsh-autosuggestions zsh-syntax-highlighting
+
+# install aactivator
+pip2 install aactivator
